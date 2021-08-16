d935784b74f5633c83dc2ef15ffe1d4401301639
kauã walter <85528638+kauawalter09@users.noreply.github.com>
2021-08-16 20:30:08 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:parte_um/jogos.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+  home: jogos(),
+  ));
+}
